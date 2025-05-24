17effec538c23f4f4e021d4ef7806a83314c1807
Natalie Carey <natalie@nowprototype.it>
2025-05-24 18:39:13 +0100

    The structure is much better, any additional work on the structure without proper content is likely to lead in the wrong direction.  The focus right now is on providing better content and this new structure should be enough to enable that.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+// This file is as I want to put my time into writing documentation and cleaning the structure up later.
+
 import path from 'node:path'
 import { promises as fsp } from 'node:fs'
 import { createRequire } from 'node:module'
-const require = createRequire(import.meta.url)
-
 import express from 'express'
-const nunjucks = require('nunjucks')
 import * as marked from 'marked'
 import { URL } from 'node:url'
+
+const require = createRequire(import.meta.url)
+
+const nunjucks = require('nunjucks')
 
 const app = express()
 const rootDir = process.cwd()
@@ -21,6 +24,7 @@ const serverErrorMdFile = path.join(errorMdDir, 'server-error.md')
 
 
 const canonicalUrl = process.env.CANONICAL_URL
+const verboseLog = process.env.NPI_DOCS__VERBOSE_LOG === 'true' ? console.log : () => {}
 const parsedCanonicalUrl = canonicalUrl ? new URL(canonicalUrl) : null
 
 const appViews = [
@@ -40,9 +44,18 @@ app.set('view engine', 'njk')
 
 async function getMarkdownContent (fileName) {
   try {
-    return marked.parse(await fsp.readFile(fileName, 'utf8'))
+    verboseLog(`Trying to read markdown file: [${fileName}]`)
+    const content = await fsp.readFile(fileName, 'utf8')
+    verboseLog(`Successfully read markdown file: [${fileName}], [${content.length} bytes], [${typeof content}]`)
+    verboseLog(content)
+    const parsed = marked.parse(content)
+    verboseLog(`Successfully parsed markdown file: [${fileName}], [${parsed.length} bytes], [${typeof parsed}]`)
+    verboseLog(parsed)
+    return parsed
   } catch (e) {
-    if (e.code !== 'ENOENT') {
+    if (e.code === 'ENOENT' || e.code === 'ENOTDIR') {
+      verboseLog(`No markdown file: [${fileName}]`)
+    } else {
       throw e
     }
   }
@@ -63,8 +76,30 @@ async function getContentFromMarkdown (urlWithoutQueryString) {
   while (fileNamesToTry.length > 0) {
     const fileName = fileNamesToTry.shift()
     const result = await getMarkdownContent(fileName)
-    if (result) {
+    if (result !== undefined) {
       return await rewriteMarkup(result)
+    }
+  }
+  const path1 = path.join(urlBasedMdDir, url)
+  try {
+    const status = await fsp.stat(path1)
+    if (status.isDirectory()) {
+      const slicedUrl = urlWithoutQueryString.split('/').slice(1)
+      const nav = await getNavModel([slicedUrl.join('/')], 'latest')
+      const foundNav = findDirectChildrenInNav(nav, slicedUrl)
+      return marked.parse(`
+# ${convertUrlNameToTitle(url.split('/').pop())}
+
+This page hasn't been written yet, but you can see the content in the sub-pages.
+
+${foundNav ? foundNav.children.map(child => `- [${child.name}](${child.url})`).join('\n') : 'No sub-pages found.'}
+`)
+      }
+  } catch (e) {
+    if (e.code === 'ENOENT' || e.code === 'ENOTDIR') {
+      verboseLog(`No markdown directory: [${path1}]`)
+    } else {
+      throw e
     }
   }
 }
@@ -82,6 +117,14 @@ app.get('/healthcheck', (req, res) => {
   res.send('Healthy')
 })
 
+app.get([
+  '/.well-known/appspecific/com.chrome.devtools.json'
+], (req, res) => {
+  res.json({
+    name: 'Now Prototype It Docs',
+  })
+})
+
 if (canonicalUrl) {
   app.use((req, res, next) => {
     if (!canonicalUrl) {
@@ -90,7 +133,6 @@ if (canonicalUrl) {
     }
     const protocol = req.headers['x-forwarded-proto']
     if (req.get('host') !== parsedCanonicalUrl.host || (protocol && protocol !== parsedCanonicalUrl.protocol.replace(':', ''))) {
-      console.log('redirecting from ' + req.get('host') + ' to ' + parsedCanonicalUrl.host + ' for ' + req.originalUrl)
       res.redirect(301, parsedCanonicalUrl.protocol + '//' + parsedCanonicalUrl.host + req.originalUrl)
     } else {
       next()
@@ -101,18 +143,31 @@ if (canonicalUrl) {
 app.use('/assets/style.css', express.static(path.join(rootDir, 'generated', 'style.css')))
 app.use('/assets/design-system', express.static(path.join(designSystemRoot, 'assets')))
 
+app.use((req, res, next) => {
+  const [url, ...qs] = req.originalUrl.split('?')
+  if (url.endsWith('/index')) {
+    res.redirect(301, [url.substring(0, url.length - '/index'.length), ...qs].join('?'))
+  } else {
+    next()
+  }
+})
+
 app.use(async (req, res, next) => {
   logPageView(req, 'request')
   try {
     const urlWithoutQueryString = req.originalUrl.split('?')[0]
-    const [version, ...urlWithoutQueryStringAndVersionParts] = urlWithoutQueryString.split('/').slice(1)
+    verboseLog(`Processing request for [${req.originalUrl}]`)
+    const [urlEncodedVersion, ...urlWithoutQueryStringAndVersionPartsBeforeFilter] = urlWithoutQueryString.split('/').slice(1)
+    const urlWithoutQueryStringAndVersionParts = urlWithoutQueryStringAndVersionPartsBeforeFilter.filter(part => part !== '')
+    const version = decodeURIComponent(urlEncodedVersion)
     if (!version || !version.startsWith('version-') && !version.startsWith('latest')) {
+      verboseLog('No version in URL, redirecting to latest version for ' + req.originalUrl)
       res.redirect('/latest' + req.originalUrl)
       return
     }
     const urlWithoutQueryStringAndVersion = '/' + urlWithoutQueryStringAndVersionParts.join('/')
     let contentFromMarkdown = await getContentFromMarkdown(urlWithoutQueryStringAndVersion)
-    if (!contentFromMarkdown) {
+    if (contentFromMarkdown === undefined) {
       res.status(404)
       contentFromMarkdown = await getMarkdownContent(notFoundMdFile)
       if (!contentFromMarkdown) {
@@ -121,17 +176,22 @@ app.use(async (req, res, next) => {
         logPageView(req, '404')
       }
     }
+    const nav = await getNavModel(urlWithoutQueryStringAndVersionParts, version)
+    const breadcrumbs = await getBreadcrumbsModel(urlWithoutQueryStringAndVersionParts)
     res.render('standard-page', {
       nowPrototypeItAssetsPath: '/assets',
       nowPrototypeItDesignSystemAssetsPath: '/assets/design-system',
       nowPrototypeItLogoLink: '/',
       contentFromMarkdown,
-      headerSubNavItems: [].map(x => ({
-        ...x,
-        isCurrentPage: x.url === urlWithoutQueryStringAndVersion
-      }))
+      breadcrumbs,
+      nav,
+      headerSubNavItems: nav.map(item => (item.name !== 'Home' ? {
+        name: item.name,
+        url: item.url,
+      } : undefined)).filter(Boolean),
     })
   } catch (e) {
+    console.error('Error while processing request for ' + req.originalUrl, e)
     next(e)
   }
 })
@@ -172,3 +232,106 @@ const listener = app.listen.apply(app, listenArgs.concat([() => {
     console.log('To view the Docs app locally visit: http://localhost:' + listener.address().port)
   }
 }]))
+
+function getRelatedToCurrentPage (fullCurrentUrl, url) {
+  return fullCurrentUrl.startsWith(url)
+}
+
+async function getNavModel(urlParts, version) {
+  const dirContents = await recursiveDirContents(urlBasedMdDir)
+  const root = [
+    {
+      name: 'Home',
+      url: '/' + encodeURIComponent(version) + '/',
+      isRelatedToCurrentPage: urlParts.length === 0,
+      isCurrentPage: urlParts.length === 0,
+    }
+  ]
+
+  function insert(parts, parent, fullPathSoFar) {
+    const [current, ...rest] = parts
+    const isFile = rest.length === 0
+    if (current === 'index.md') {
+      return
+    }
+    let node = parent.find(n => n.name === convertUrlNameToTitle(current))
+    const fullCurrentUrl = ['', encodeURIComponent(version), ...(urlParts)].join('/')
+    if (!node) {
+      const name = convertUrlNameToTitle(current.replace(/\.md$/, ''))
+      const url = '/' + encodeURIComponent(version) + '/' + [...fullPathSoFar, current.replace(/\.md$/, '')].join('/')
+      node = {
+        name,
+        url,
+        isRelatedToCurrentPage: getRelatedToCurrentPage(fullCurrentUrl, url),
+        isCurrentPage: fullCurrentUrl === url,
+      }
+      if (!isFile) node.children = []
+      parent.push(node)
+    }
+    if (!isFile) {
+      insert(rest, node.children, [...fullPathSoFar, current])
+    }
+  }
+
+  dirContents.forEach(filePath => {
+    const parts = filePath.split('/')
+    insert(parts, root, [])
+  })
+
+  function clean(node) {
+    if (node.children) {
+      node.children = node.children.map(clean).filter(Boolean)
+      if (node.children.length === 0) delete node.children
+    }
+    return node
+  }
+
+  return root.map(clean)
+}
+async function getBreadcrumbsModel (urlParts) {
+  return [{name: 'Home', url: '/'}].concat(urlParts.map((part, index) => {
+    const url = '/' + urlParts.slice(0, index + 1).join('/')
+    return {
+      name: convertUrlNameToTitle(part),
+      url
+    }
+  }))
+}
+
+function convertUrlNameToTitle (urlName) {
+  return urlName
+    .replace(/-/g, ' ')
+    .split(' ')
+    .map(capitaliseWord)
+    .join(' ')
+}
+
+function capitaliseWord(word) {
+  if (word === 'a') {
+    return word
+  }
+  return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+async function recursiveDirContents (dir) {
+  const entries = await fsp.readdir(dir, { withFileTypes: true })
+  const results = []
+  for (const entry of entries) {
+    const fullPath = path.join(dir, entry.name)
+    if (entry.isDirectory()) {
+      results.push(...(await recursiveDirContents(fullPath)).map(x => path.join(entry.name, x)))
+    } else if (entry.isFile() && entry.name.endsWith('.md')) {
+      results.push(path.relative(dir, fullPath))
+    }
+  }
+  return results
+}
+
+function findDirectChildrenInNav(nav, slicedUrl) {
+  const currentPart = slicedUrl.shift()
+  const found = nav.find(item => item.name === convertUrlNameToTitle(currentPart))
+  if (found && slicedUrl.length > 0) {
+    return findDirectChildrenInNav(found.children || [], slicedUrl)
+  }
+  return found
+}
--- views/url-based/index.md
+++ views/url-based/index.md
@@ -1,6 +1,10 @@
-# Hello world!
+# Now Prototype It Documentation
 
-We're currently working on our documentation.  This site is where you'll find it when it's ready.
+Welcome, we're in the process of writing documentation for Now Prototype It.  Please use the nav menu to find the content you're looking for
 
-For now, if you have any questions please [email us at support@nowprototype.it](mailto:support@nowprototype.it) or
-[raise an issue on our GitHub project](https://github.com/nowprototypeit/govuk/issues).
+If you have any questions, please
+
+ - [Email us at support@nowprototype.it](mailto:support@nowprototype.it) or
+ - [Raise an issue on our GitHub project](https://github.com/nowprototypeit/govuk/issues).
+
+[This documentation project is open source, so if you want to contribute, please do!](https://github.com/nowprototypeit/docs/blob/main/README.md)
