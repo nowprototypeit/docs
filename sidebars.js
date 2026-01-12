aff5e65920d73762b8fa6da07b42759cbce52ad6
Siobhan Fitzgerald-Gibson <siobhang64@gmail.com>
2026-01-12 15:04:05 +0000

    added link-pages and sharing-prototypes

--- sidebars.js
+++ sidebars.js
@@ -26,6 +26,7 @@ const sidebars = {
       items: [
         'prototypes/index',
         'prototypes/create-prototype',
+        'prototypes/link-pages'
         'prototypes/run-prototype-locally',
         'prototypes/create-prototype-pages',
         'prototypes/edit-prototype-pages',
