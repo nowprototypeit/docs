40cadf13634dde24fe7917e9f230bd1d47251216
Natalie Carey <natalie@nataliecarey.uk>
2026-01-17 01:05:04 +0000

    Fixed burger menu.

--- src/theme/Navbar/Layout/index.tsx
+++ src/theme/Navbar/Layout/index.tsx
@@ -11,11 +11,48 @@ import type {Props} from '@theme/Navbar/Layout';
 
 import styles from './styles.module.css';
 
-export default function NavbarLayout({children}: Props): ReactNode {
+function NavbarBackdrop(props: ComponentProps<'div'>) {
   return (
-	  <header className="navbar">
-	    <h3><span className="visually-hidden">Now Prototype It</span> Documentation</h3>
-	    {children}
-	  </header>
+    <div
+      role="presentation"
+      {...props}
+      className={clsx('navbar-sidebar__backdrop', props.className)}
+    />
+  );
+}
+
+export default function NavbarLayout({children}: Props): ReactNode {
+  const {
+    navbar: {hideOnScroll, style},
+  } = useThemeConfig();
+  const mobileSidebar = useNavbarMobileSidebar();
+  const {navbarRef, isNavbarVisible} = useHideableNavbar(hideOnScroll);
+  return (
+    <nav
+      ref={navbarRef}
+      aria-label={translate({
+        id: 'theme.NavBar.navAriaLabel',
+        message: 'Main',
+        description: 'The ARIA label for the main navigation',
+      })}
+      className={clsx(
+        ThemeClassNames.layout.navbar.container,
+        'navbar',
+        'navbar--fixed-top',
+        hideOnScroll && [
+          styles.navbarHideable,
+          !isNavbarVisible && styles.navbarHidden,
+        ],
+        {
+          'navbar--dark': style === 'dark',
+          'navbar--primary': style === 'primary',
+          'navbar-sidebar--show': mobileSidebar.shown,
+        },
+      )}>
+      {children}
+      <NavbarBackdrop onClick={mobileSidebar.toggle} />
+      <h3><span className="visually-hidden">Now Prototype It</span> Documentation</h3>
+      <NavbarMobileSidebar />
+    </nav>
   );
 }
